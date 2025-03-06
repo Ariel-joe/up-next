dcfa81024b8fb8f11efa1a2413f2110165e218d2
Ariel-Joe <arieljoe00@gmail.com>
2025-03-06 22:24:38 +0300

    lanyard added successsfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Lanyard from "@/components/ui/lanyard";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>hello up-next</h1>        
+      <h1>hello up-next</h1>
+      <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+        
     </div>
   );
 }
